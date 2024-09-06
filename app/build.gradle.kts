749e91a6b75b11cd2ca25d14372e7b4bce42eca4
raheem <raheemideadev@gmail.com>
2024-09-06 12:10:42 +0500

    Installed dependencies;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.android)
+    alias(libs.plugins.kotlin)
 }
 
 android {
-    namespace = "xyz.temgravity.productflavordemo"
-    compileSdk = 34
+    namespace = "xyz.teamgravity.productflavordemo"
+    compileSdk = libs.versions.sdk.compile.get().toInt()
 
     defaultConfig {
-        applicationId = "xyz.temgravity.productflavordemo"
-        minSdk = 26
-        targetSdk = 34
+        applicationId = "xyz.teamgravity.productflavordemo"
+        minSdk = libs.versions.sdk.min.get().toInt()
+        targetSdk = libs.versions.sdk.target.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
@@ -23,23 +23,31 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = libs.versions.java.target.get()
     }
 }
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    // core
+    implementation(libs.core)
+
+    // appcompat
+    implementation(libs.appcompat)
+
+    // material
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    // constraint layout
+    implementation(libs.constraintlayout)
+
+    // activity
+    implementation(libs.activity)
 }
